92aa3157212fb0b8448f9c6b58ec4cd7072ac2e1
Criativaria <mariaritaboareto@gmail.com>
2024-05-11 00:53:30 -0300

    front complete

--- backend/src/prisma.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient(); //acesso ao banco de dados
--- backend/src/routes/user-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { UserController } from "../controllers/user-controllers";
-
-const userRouter = Router()
-
-userRouter.get('/score', UserController.getUserScore)
-userRouter.post('/signIn', UserController.signIn)
-
-export { userRouter }
